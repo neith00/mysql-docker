13f4ebb815a269a39766aa46a33109a68d6b74f4
neith00 <ja.lauricella@gmail.com>
2017-12-30 12:45:15 +0100

    moved mysql init complete to /tmp

--- 5.7/healthcheck.sh
+++ 5.7/healthcheck.sh
@@ -16,7 +16,7 @@
 
 # The mysql-init-complete file is touched by the entrypoint file before the
 # main server process is started
-if [ -f /var/lib/mysql/mysql-init-complete ]; # The entrypoint script touches this file
+if [ -f /tmp/mysql-init-complete ]; # The entrypoint script touches this file
 then # Ping server to see if it is ready
   mysqladmin --defaults-extra-file=/var/lib/mysql/healthcheck.cnf ping
 else # Initialization still in progress
