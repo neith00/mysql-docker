7955d644e01c6262ddaa9a22cf61e62aaba9a407
neith00 <ja.lauricella@gmail.com>
2017-12-30 12:15:14 +0100

    fixed mysql init path in healthcheck.sh

--- 5.7/healthcheck.sh
+++ 5.7/healthcheck.sh
@@ -16,8 +16,8 @@
 
 # The mysql-init-complete file is touched by the entrypoint file before the
 # main server process is started
-if [ -f /mysql-init-complete ]; # The entrypoint script touches this file
-then # Ping server to see if it is ready
+if [ -f /var/lib/mysql/mysql-init-complete ]; # The entrypoint script touches this file
+then # P/var/lib/mysql/ing server to see if it is ready
   mysqladmin --defaults-extra-file=/healthcheck.cnf ping
 else # Initialization still in progress
   exit 1
